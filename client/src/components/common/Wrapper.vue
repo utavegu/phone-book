5f558a26e6a7f28fc727832b8cee1ad44d52df6c
Utavegu <utavegu@list.ru>
2023-12-16 12:00:55 +0500

    feat: add main layout and wrapper

---
+++ client/src/components/common/Wrapper.vue
@@ -0,0 +1,19 @@
+<template>
+  <div className="wrapper">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  margin: 0 auto;
+  padding: 0 24px;
+  max-width: 1440px;
+}
+
+@media (min-width: 1440px) {
+  .wrapper {
+    padding: 0 116px;
+  }
+}
+</style>
---
+++ client/src/layouts/main-layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <div class="layout">
+      <common-header />
+      <main>
+        <slot />
+      </main>
+      <common-footer />
+    </div>
+  </v-app>
+</template>
+
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  overflow: hidden;
+}
+</style>
