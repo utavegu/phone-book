fd7c59999f078f65c34a6e53e78a499872c2d864
Utavegu <utavegu@list.ru>
2023-12-16 12:02:02 +0500

    feat: add header and footer

---
+++ client/src/components/common/Footer.vue
@@ -0,0 +1,233 @@
+<template>
+  <footer class="main-footer">
+    <common-wrapper>
+      <div class="first-line">
+        <div class="logo-block">
+          <NuxtLink to="/">
+            <img
+              src="https://via.placeholder.com/112x72"
+              alt="Логотип"
+              width="112"
+              height="72"
+            />
+          </NuxtLink>
+        </div>
+
+        <div class="nav-block">
+          <ul>
+            <li>
+              <NuxtLink to="/">Страница 1</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/">Страница 2</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/">Страница 3</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/">Страница 4</NuxtLink>
+            </li>
+            <li class="separator">|</li>
+            <li class="phone"><a href="tel:8 800 111-22-33">8 800 111-22-33</a></li>
+          </ul>
+        </div>
+
+        <div class="social-block">
+          <ul class="social-list">
+            <li class="social-item">
+              <img
+                src="https://via.placeholder.com/32x32.png"
+                alt="Социальная сеть такая-то"
+                title="Социальная сеть такая-то"
+              />
+            </li>
+            <li class="social-item">
+              <img
+                src="https://via.placeholder.com/32x32.png"
+                alt="Социальная сеть такая-то"
+                title="Социальная сеть такая-то"
+              />
+            </li>
+            <li class="social-item">
+              <img
+                src="https://via.placeholder.com/32x32.png"
+                alt="Социальная сеть такая-то"
+                title="Социальная сеть такая-то"
+              />
+            </li>
+            <li class="social-item">
+              <img
+                src="https://via.placeholder.com/32x32.png"
+                alt="Социальная сеть такая-то"
+                title="Социальная сеть такая-то"
+              />
+            </li>
+            <li class="social-item">
+              <img
+                src="https://via.placeholder.com/32x32.png"
+                alt="Социальная сеть такая-то"
+                title="Социальная сеть такая-то"
+              />
+            </li>
+            <li class="social-item">
+              <img
+                src="https://via.placeholder.com/32x32.png"
+                alt="Социальная сеть такая-то"
+                title="Социальная сеть такая-то"
+              />
+            </li>
+            <li class="social-item">
+              <img
+                src="https://via.placeholder.com/32x32.png"
+                alt="Социальная сеть такая-то"
+                title="Социальная сеть такая-то"
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="second-line">
+        <div class="offer-block">
+          <NuxtLink to="/user-agreement">Пользовательское соглашение</NuxtLink>
+          <span>Политика конфиденциальности</span>
+        </div>
+
+        <div class="copyright-block">© 2023 Все права защищены</div>
+      </div>
+    </common-wrapper>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+.main-footer {
+  margin-top: auto;
+  padding: 48px 0 80px;
+  border-top: 1px solid var(--strip-color);
+  font: var(--footer);
+  text-align: center;
+}
+
+.logo-block {
+  margin: 0 auto 40px;
+  max-width: 200px;
+  height: auto;
+
+  & > img {
+    object-fit: contain;
+  }
+}
+
+.nav-block {
+  display: none;
+}
+
+.social-block {
+  margin-bottom: 60px;
+}
+
+.social-list {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 12px;
+  justify-content: center;
+}
+
+.social-item {
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  overflow: hidden;
+
+  & > img {
+    object-fit: cover;
+  }
+}
+
+.offer-block {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 48px;
+  column-gap: 20px;
+
+  & > a {
+    color: inherit;
+    &:hover {
+      color: var(--text-color-hover);
+    }
+  }
+}
+
+.copyright-block {
+  opacity: 0.5;
+}
+
+@media (min-width: 768px) {
+  .first-line {
+    margin-bottom: 33px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .second-line {
+    display: flex;
+    column-gap: 20px;
+  }
+
+  .nav-block {
+    display: block;
+    max-width: 700px;
+    margin: 0 auto 0 40px;
+    font: var(--text);
+
+    & ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      flex-wrap: wrap;
+      column-gap: 32px;
+    }
+
+    & a {
+      color: inherit;
+      &:hover {
+        color: var(--text-color-hover);
+      }
+    }
+  }
+
+  .separator {
+    opacity: 0.2;
+  }
+
+  .offer-block {
+    margin-bottom: 0;
+    order: 1;
+  }
+
+  .logo-block {
+    margin: 0;
+    flex-shrink: 0;
+    & img {
+      display: block;
+    }
+  }
+
+  .social-block {
+    margin-bottom: 0;
+  }
+}
+
+@media (min-width: 1440px) {
+  .main-footer {
+    padding: 35px 0;
+    font: var(--footer);
+    text-align: left;
+  }
+}
+</style>
---
+++ client/src/components/common/Header.vue
@@ -0,0 +1,249 @@
+<script lang="ts">
+export default {
+  data: () => ({
+    isShowMenu: false,
+  }),
+  methods: {
+    handleShowMenu() {
+      this.isShowMenu = !this.isShowMenu;
+    },
+    handleEnterButtonClick() {},
+    handleRegisterButtonClick() {},
+  },
+};
+</script>
+
+<template>
+  <header class="main-header">
+    <common-wrapper>
+      <div class="logo-block">
+        <div class="logo-container">
+          <NuxtLink to="/">
+            <img
+              src="https://via.placeholder.com/112x72"
+              alt="Логотип"
+              width="112"
+              height="72"
+            />
+          </NuxtLink>
+        </div>
+        <button
+          :class="['button-close', isShowMenu ? 'cross' : 'burger']"
+          @click="handleShowMenu"
+        >
+          <span class="visually-hidden">Закрыть</span>
+        </button>
+      </div>
+
+      <nav
+        v-show="isShowMenu"
+        class="header-navigation"
+      >
+        <ul>
+          <li>
+            <NuxtLink to="/">Страница 1</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Страница 2</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Страница 3</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/">Страница 4</NuxtLink>
+          </li>
+          <li class="separator">|</li>
+          <li class="phone"><a href="tel:8 800 111-22-33">8 800 111-22-33</a></li>
+        </ul>
+      </nav>
+
+      <div
+        v-show="isShowMenu"
+        class="user-menu"
+      >
+        <ui-button
+          class="header-button button-enter"
+          variant="outlined"
+          @click="handleEnterButtonClick"
+        >
+          Войти
+        </ui-button>
+        <ui-button
+          class="header-button button-register"
+          variant="colored-accent"
+          @click="handleRegisterButtonClick"
+        >
+          Зарегистрироваться
+        </ui-button>
+      </div>
+    </common-wrapper>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.main-header {
+  font: var(--links-forms);
+  overflow-x: hidden;
+}
+
+.logo-block {
+  position: relative;
+  min-height: 96px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  &::before {
+    position: absolute;
+    content: '';
+    bottom: 0;
+    left: -100%;
+    width: 300%;
+    height: 1px;
+    background: var(--line-color);
+  }
+}
+
+.logo-container {
+  max-width: 200px;
+  height: auto;
+  padding-top: 10px;
+  padding-bottom: 5px;
+
+  & > img {
+    display: block;
+    object-fit: contain;
+  }
+}
+
+.button-close {
+  width: 28px;
+  height: 24px;
+  border: none;
+  cursor: pointer;
+  background: center no-repeat;
+}
+
+.cross {
+  background-image: url(~/assets/icons/green-cross.svg);
+}
+
+.burger {
+  background-image: url(~/assets/icons/green-burger.svg);
+}
+
+.header-navigation {
+  margin: 40px 0 40px;
+
+  & ul {
+    margin: 0 auto;
+    padding: 0;
+    list-style: none;
+    text-align: center;
+  }
+
+  & li:not(:last-child) {
+    margin-bottom: 32px;
+  }
+
+  & a {
+    color: inherit;
+
+    &:hover {
+      color: var(--text-color-hover);
+    }
+  }
+}
+
+.separator {
+  display: none;
+}
+
+.phone {
+  font: var(--buttons);
+}
+
+.user-menu {
+  margin-bottom: 32px;
+}
+
+.header-button {
+  width: 100%;
+}
+
+.button-enter {
+  margin-bottom: 20px;
+}
+
+@media (min-width: 1100px) {
+  .main-header {
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  }
+
+  .main-header > div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    min-height: 96px;
+  }
+
+  .logo-block {
+    min-height: auto;
+
+    &::before {
+      display: none;
+    }
+  }
+
+  .button-close {
+    display: none;
+  }
+
+  .header-navigation {
+    display: block !important;
+    margin: 0 0 0 5px;
+    max-width: 700px;
+
+    & ul {
+      display: flex;
+      flex-wrap: wrap;
+      text-align: left;
+    }
+
+    & li:not(:last-child) {
+      margin: 0 32px 0 0;
+    }
+  }
+
+  .separator {
+    display: block;
+    opacity: 0.2;
+  }
+
+  .user-menu {
+    display: flex !important;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 0;
+    width: 293px;
+  }
+
+  .header-button {
+    width: auto;
+    height: 48px;
+  }
+
+  .button-enter {
+    min-width: auto;
+    margin-bottom: 0;
+    padding: 0;
+    border: none;
+  }
+
+  .button-register {
+    min-width: 206px;
+  }
+}
+</style>
