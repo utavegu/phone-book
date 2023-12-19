8d632b824cc96a17ebf77d63f71471a26140d0fd
Utavegu <utavegu@list.ru>
2023-12-19 22:35:30 +0500

    build: install eslint and set up linters

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  extends: ['@nuxtjs/eslint-config-typescript', 'plugin:prettier/recommended'],
+  plugins: [],
+  rules: {
+    // Эти алиасы не люблю, так как они код делают менее читабельным и единообразным
+    'vue/v-on-style': 'off',
+    'vue/v-bind-style': 'off',
+    // А это правило не актуально в Nuxt, так как он компоненты автоматически делает multi-word, добавляя префиксом имя директории их расположения
+    'vue/multi-word-component-names': 'off',
+  },
+};
